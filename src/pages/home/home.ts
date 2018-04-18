c2d4df69b91dd39033172b7f3cf7b5406f253111
Oscar <oscar101019@gmail.com>
2018-04-18 09:04:34 -0600

    Pantallas principales

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PerfilPage } from "../perfil/perfil";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public usuario={
+    email:''
+  }
 
+  constructor(public navCtrl: NavController ) {
+
+  }
+
+  ingresarDetalles(){
+    this.navCtrl.push(PerfilPage)
   }
 
 }
